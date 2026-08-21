bcc758940f2641f43f2cff5e772f24a3c1f060cb
Sumaiya Khan <maboodahmadkhan6@gmail.com>
2026-08-22 00:39:18 +0530

    Time: 0 ms (100%), Space: 8.6 MB (32.44%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -5,11 +5,10 @@ public:
        int m = t.length();
        int i=0;
        for(int j=0 ; j<m ; j++){
-        if(s[i] == t[j]){
+        if(i < n && s[i] == t[j]){
             i++;
         }
        }
-       if(i == n)return true;
-       else return false;
+        return i == n;
     }
 };
