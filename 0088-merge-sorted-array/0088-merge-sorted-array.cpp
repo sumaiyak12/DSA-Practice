a20d9f55ed243a4fbceb3f1f396c5b1e5de8b96f
Sumaiya Khan <maboodahmadkhan6@gmail.com>
2026-09-02 00:57:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (80.65%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,52 +1,25 @@
 class Solution {
-private:
-    void swapIfGreater(vector<int>& nums1,
-                       vector<int>& nums2,
-                       int idx1,
-                       int idx2) {
-        if(nums1[idx1] > nums2[idx2]) {
-            swap(nums1[idx1], nums2[idx2]);
-        }
-    }
-
 public:
-    void merge(vector<int>& nums1, int m,
-               vector<int>& nums2, int n) {
+    void merge(vector<int>& arr1, int m, vector<int>& arr2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        int len = m + n;
-        int gap = (len / 2) + (len % 2);
-
-        while(gap > 0) {
-
-            int left = 0;
-            int right = left + gap;
-
-            while(right < len) {
-
-                if(left < m && right >= m) {
-                    swapIfGreater(nums1, nums2,
-                                  left, right - m);
-                }
-                else if(left >= m) {
-                    swapIfGreater(nums2, nums2,
-                                  left - m, right - m);
-                }
-                else {
-                    swapIfGreater(nums1, nums1,
-                                  left, right);
-                }
-
-                left++;
-                right++;
+        while (i >= 0 && j >= 0) {
+            if (arr1[i] > arr2[j]) {
+                arr1[k] = arr1[i];
+                i--;
+            } else {
+                arr1[k] = arr2[j];
+                j--;
             }
-
-            if(gap == 1) break;
-
-            gap = (gap / 2) + (gap % 2);
+            k--;
         }
 
-        for(int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
+        while (j >= 0) {
+            arr1[k] = arr2[j];
+            j--;
+            k--;
         }
     }
 };
